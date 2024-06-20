9df11d715ae6dba5f5fa4e04c9f9861ea8b07cec
riznanasar003 <richurizna10@gmail.com>
2024-06-20 22:31:47 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+    </Routes>
+   </BrowserRouter>
   );
 }
 
